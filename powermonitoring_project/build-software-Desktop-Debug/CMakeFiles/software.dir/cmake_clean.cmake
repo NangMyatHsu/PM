14df2ff2891cabd63c9e9e783a35e631912734e3
yoyoasaw10 <116140433+yoyoasaw10@users.noreply.github.com>
2025-06-13 11:29:11 +0630

    Add files via upload
    yoyo

---
+++ powermonitoring_project/build-software-Desktop-Debug/CMakeFiles/software.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/software_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/software_autogen.dir/ParseCache.txt"
+  "software_autogen"
+  "CMakeFiles/software.dir/main.cpp.o"
+  "CMakeFiles/software.dir/main.cpp.o.d"
+  "CMakeFiles/software.dir/mainwindow.cpp.o"
+  "CMakeFiles/software.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/software.dir/software_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/software.dir/software_autogen/mocs_compilation.cpp.o.d"
+  "software"
+  "software.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/software.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
